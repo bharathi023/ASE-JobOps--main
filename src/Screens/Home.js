a93d7d6bff79fd530a8c9d3e85a66d902a232c9a
Putta23 <31534695+bharathi023@users.noreply.github.com>
2023-08-12 08:57:31 -0500

    Update Home.js

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="banner" >
       <div>
       
-        <h2>Welcome Gayathri Annam ! Looking for a job? </h2>
+        <h2>Welcome! Looking for a job? </h2>
         <h3 style={{color:"white"}}>
           You are at the right place. Find Part Time and Full time jobs from different sectors.
         </h3>
